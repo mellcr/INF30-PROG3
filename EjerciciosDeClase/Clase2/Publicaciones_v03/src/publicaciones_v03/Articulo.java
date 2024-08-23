7b750e104f4b919e1b02065db968aed229e61b1a
meluca <mell171103@gmail.com>
2024-08-23 11:35:06 -0500

    Creacion del proyecto trabajado en clase

---
+++ EjerciciosDeClase/Clase2/Publicaciones_v03/src/publicaciones_v03/Articulo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package publicaciones_v03;
+
+/**
+ *
+ * @author mell1
+ */
+public class Articulo {
+    
+}
---
+++ EjerciciosDeClase/Clase2/Publicaciones_v03/src/publicaciones_v03/Publicaciones_v03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package publicaciones_v03;
+
+/**
+ *
+ * @author mell1
+ */
+public class Publicaciones_v03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
